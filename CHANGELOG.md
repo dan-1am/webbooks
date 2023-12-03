a0e4cb1f6746265e6c312375a9943c09aef72765
Denis Orekhov <oda16m@gmail.com>
2023-12-03 14:35:47 +0700

    Set up admin interface.

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Django Library Changelog
+
+## 2023.12.03
+Set up admin interface.
+
+## 2023.11.29
+Create views for authors, sequences, book, read.
+Added book images.
--- admin.py
+++ admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book,Author,Sequence,Genre
+
+class BookToAuthorInline(admin.TabularInline):
+    model = Book.authors.through
+    extra = 1
+
+class BookAdmin(admin.ModelAdmin):
+    inlines = [BookToAuthorInline]
+    exclude = ["authors"]
+    search_fields = ["title"]
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ["name", "books"]
+    inlines = [BookToAuthorInline]
+    search_fields = ["name"]
+    @admin.display(description="Books")
+    def books(self, author):
+        text = ",".join(b["title"] for b in author.book_set.values("title").all())
+        return text
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Sequence)
+admin.site.register(Genre)
