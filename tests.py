3a32e8c5733710e6747e6403997f0191b761c4d9
Denis Orekhov <oda16m@gmail.com>
2024-09-15 10:21:36 +0700

    Add django test stub.

--- tests.py
+++ tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import *
+
+
+
+class TestWebbooks(TestCase):
+    databases = "__all__"
+
+    def setUp(self):
+        Book.objects.create(title="Title1", file="file1.fb2", hash="hash1")
+        Book.objects.create(title="Title2", file="file2.fb2", hash="hash2")
+
+    def test1(self):
+        book = Book.objects.get(title__contains="2")
+        self.assertEqual(book.title, "Title2")
