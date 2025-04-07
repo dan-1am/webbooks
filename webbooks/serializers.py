29d18448f1786dedd4e56157486d1b7fd1888d7a
Denis Orekhov <oda16m@gmail.com>
2025-04-07 14:38:34 +0700

    Subclass HyperlinkedModelSerializer for automatic scoped view names.

--- webbooks/serializers.py
+++ webbooks/serializers.py
@@ -4,50 +4,36 @@ from rest_framework import serializers
 from webbooks.models import Genre,Author,Sequence,Book,Comment
 
 
-def view_name(cls):
-    app_label = cls._meta.app_label
-    model_name = cls._meta.model_name
-    return f"{app_label}:{model_name}-detail"
+
+class ScopedSerializer(serializers.HyperlinkedModelSerializer):
+    """Add "app_name:" scope to view name for related fields."""
+    def build_field(self, *args, **kwargs):
+        field_class,field_kwargs = super().build_field(*args, **kwargs)
+        if 'view_name' in field_kwargs:
+            app_name = self.Meta.model._meta.app_label
+            field_kwargs['view_name'] = f"{app_name}:{field_kwargs['view_name']}"
+        return field_class, field_kwargs
 
 
-def linked_id_field(cls):
-    return serializers.HyperlinkedIdentityField(view_name=view_name(cls))
-
-
-def linked_related_field(cls, many=False):
-    return serializers.HyperlinkedRelatedField(
-        many=many,
-        view_name=view_name(cls),
-        queryset=cls.objects.all()
-    )
-
-
-class AuthorSerializer(serializers.HyperlinkedModelSerializer):
-    url = linked_id_field(Author)
+class AuthorSerializer(ScopedSerializer):
     class Meta:
         model = Author
         fields = ['url', 'id', 'name']
 
 
-class GenreSerializer(serializers.HyperlinkedModelSerializer):
-    url = linked_id_field(Genre)
+class GenreSerializer(ScopedSerializer):
     class Meta:
         model = Genre
         fields = ['url', 'id', 'name']
 
 
-class SequenceSerializer(serializers.HyperlinkedModelSerializer):
-    url = linked_id_field(Sequence)
+class SequenceSerializer(ScopedSerializer):
     class Meta:
         model = Sequence
         fields = ['url', 'id', 'name']
 
 
-class BookSerializer(serializers.HyperlinkedModelSerializer):
-    url = linked_id_field(Book)
-    authors = linked_related_field(Author, True)
-    genres = linked_related_field(Genre, True)
-    sequence = linked_related_field(Sequence)
+class BookSerializer(ScopedSerializer):
     class Meta:
         model = Book
         fields = ['url', 'id', 'title', 'authors', 'genres', 'date',
