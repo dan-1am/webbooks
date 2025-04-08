7562ebb628b6683c6aa7ad3d73cc1f66da995d7a
Denis Orekhov <oda16m@gmail.com>
2025-04-08 14:56:08 +0700

    Complete API tests.

--- tests/tests_api.py
+++ tests/tests_api.py
@@ -2,6 +2,7 @@ import json
 
 from django.test import TestCase
 from django.urls import reverse
+from rest_framework import status
 
 from webbooks.models import Book
 
@@ -39,6 +40,45 @@ class WebbooksAPITest(TestCase):
         response = self.client.get(self.book_list_url)
         data_list = json.loads( response.content.decode("utf8") )
         data_list = remove_paginator(data_list)
-        data_dict = {item['id']: item for item in data_list}
+        data_dict = {item["id"]: item for item in data_list}
         for book in created:
-            self.assertEqual(book.title, data_dict[book.id]['title'])
+            self.assertEqual(book.title, data_dict[book.id]["title"])
+
+    def action_create(self, url, data):
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        return response
+
+    def action_read(self, url, data):
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        received = remove_paginator(response.data)
+        if isinstance(data, dict):
+            for k in data:
+                self.assertEqual(data[k], received[k])
+        else:
+            for a,b in zip(data, received):
+                for k in a:
+                    self.assertEqual(a[k], b[k])
+
+    def view_actions(self, model, data):
+        response = self.action_create(reverse(f"webbooks:{model}-list"), data)
+        pk = response.data["id"]
+        self.action_read(reverse(f"webbooks:{model}-list"), [data])
+        self.action_read(reverse(f"webbooks:{model}-detail", args=[pk]), data)
+
+    def test_author_views(self):
+        data = {"name": "Author 1"}
+        self.view_actions("author", data)
+
+    def test_genre_views(self):
+        data = {"name": "genre 1"}
+        self.view_actions("genre", data)
+
+    def test_sequence_views(self):
+        data = {"name": "sequence 1"}
+        self.view_actions("sequence", data)
+
+    def test_book_views(self):
+        data = {"title": "book 1", "file": "file1", "hash": "abcd"}
+        self.view_actions("book", data)
