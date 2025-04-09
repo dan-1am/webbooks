928bd1f8acca7764fb2158f87aa563100adae3d0
Denis Orekhov <oda16m@gmail.com>
2025-04-09 17:48:47 +0700

    Add ordering to API views.

--- webbooks/apiviews.py
+++ webbooks/apiviews.py
@@ -6,21 +6,21 @@ import webbooks.serializers as s
 
 
 class AuthorViewSet(viewsets.ModelViewSet):
-    queryset = m.Author.objects.all()
+    queryset = m.Author.objects.order_by("id")
     serializer_class = s.AuthorSerializer
 
 class GenreViewSet(viewsets.ModelViewSet):
-    queryset = m.Genre.objects.all()
+    queryset = m.Genre.objects.order_by("id")
     serializer_class = s.GenreSerializer
 
 class SequenceViewSet(viewsets.ModelViewSet):
-    queryset = m.Sequence.objects.all()
+    queryset = m.Sequence.objects.order_by("id")
     serializer_class = s.SequenceSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
-    queryset = m.Book.objects.all()
+    queryset = m.Book.objects.order_by("id")
     serializer_class = s.BookSerializer
 
 class FullBookViewSet(viewsets.ModelViewSet):
-    queryset = m.Book.objects.prefetch_related()
+    queryset = m.Book.objects.order_by("id").prefetch_related()
     serializer_class = s.FullBookSerializer
